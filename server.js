271a1db687099e032d2f6e3784005641b5fc595e
wchristian28 <wchristianx22@gmail.com>
2022-07-28 15:15:48 -0500

    more fixes

--- server.js
+++ server.js
@@ -1,47 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
+const express = require('express')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors');
+const { response } = require('express');
+const register = require('./controlers/register');
+const signin = require('./controlers/signin');
+const profile = require ('./controlers/profile');
+const image = require('./controlers/image');
 const knex = require('knex');
-
-const register = require('./controllers/register');
-const signin = require('./controllers/signin')
-const profile = require('./controllers/profile')
-const image = require('./controllers/image')
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,                            
+      connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false
       }
-  
     }
-});
-
-
-// db.select('*').from('users').then(data =>{
-// 	console.log(data);
-// });
-
+  });
+  
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
 
-app.get('/', (req, res)=> { res.send('it is working')})
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})   
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.post('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
+app.get('/', (req, res) => {res.send('it is working');})
+app.post ('/signin', (req, res )=> {signin.handleSignin(req, res, db, bcrypt)} )
+app.post('/register', (req, res) => {register.handleRegister(req ,res, db, bcrypt)})
+app.get('/profile/:id', (req, res)=> {profile.handleProfile(req, res, db)} )
+app.put('/image', (req,res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
 
 
 app.listen(process.env.PORT || 3000, ()=> {
     console.log(`app is running on port ${process.env.PORT}`);
 })
+
 
