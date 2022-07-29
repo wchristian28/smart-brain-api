de6f9c0900c51a549753470fcfe1d9d8b2ed3a64
wchristian28 <wchristianx22@gmail.com>
2022-07-29 11:09:53 -0500

    next fix

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors());
 
 
 
-app.get('/', (req, res) => {res.send('it is working');})
+app.get('/', (req, res) => {res.send('it is working')})
 app.post ('/signin', (req, res )=> {signin.handleSignin(req, res, db, bcrypt)} )
 app.post('/register', (req, res) => {register.handleRegister(req ,res, db, bcrypt)})
 app.get('/profile/:id', (req, res)=> {profile.handleProfile(req, res, db)} )
