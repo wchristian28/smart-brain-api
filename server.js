513328aa6ae22bfe18a982cafa395d8d797a3149
wchristian28 <wchristianx22@gmail.com>
2022-07-29 11:35:06 -0500

    fixed port and old ref db

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(cors());
 
 
 
-app.get('/', (req, res) => {res.send('it is working')})
+app.get("/", (req, res) => {
+  res.send("it's working!");
+  });
 app.post ('/signin', (req, res )=> {signin.handleSignin(req, res, db, bcrypt)} )
 app.post('/register', (req, res) => {register.handleRegister(req ,res, db, bcrypt)})
 app.get('/profile/:id', (req, res)=> {profile.handleProfile(req, res, db)} )
@@ -35,8 +37,7 @@ app.put('/image', (req,res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
 
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`app is running on port ${process.env.PORT}`);
-})
-
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+  });
 
